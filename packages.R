246f16acc06226355a41a0e6ff9198615c1b3ba1
Varun Satish <varun.satish@princeton.edu>
2024-06-21 08:52:35 -0400

    added caret

--- packages.R
+++ packages.R
@@ -1,3 +1,3 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("dplyr","data.table","tidyr"), repos="https://cran.r-project.org", dependencies=TRUE)
+install.packages(c("dplyr","data.table","tidyr", "caret"), repos="https://cran.r-project.org", dependencies=TRUE)

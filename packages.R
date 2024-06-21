c5069d4a28530dd4568ce836ec9ac5e8caa7a116
Varun Satish <varun.satish@princeton.edu>
2024-06-21 09:26:17 -0400

    added tidyverse to packages

--- packages.R
+++ packages.R
@@ -1,3 +1,3 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("dplyr","data.table","tidyr", "caret"), repos="https://cran.r-project.org", dependencies=TRUE)
+install.packages(c("dplyr","data.table","tidyr", "caret", "tidyverse"), repos="https://cran.r-project.org", dependencies=TRUE)
